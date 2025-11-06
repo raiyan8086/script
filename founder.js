c41dbaf45d4ad10186f3f59bc1654a5600313dc6
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2025-11-06 17:50:39 +0600

    Update founder.js

--- founder.js
+++ founder.js
@@ -1,1 +1,10 @@
 console.log('Load Success')
+
+const arg = process.argv[2]
+
+console.log('Received arg:', arg)
+
+process.on('message', (data) => {
+    console.log('From index:', data)
+    // process.send(JSON.stringify({"s":300}))
+})
