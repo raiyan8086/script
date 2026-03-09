54ab3bca1e26cc4b8c8ecdeb09354fdb15aee088
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2026-03-10 02:21:26 +0600

    Update founder.js

--- founder.js
+++ founder.js
@@ -242,7 +242,7 @@ async function parallelRequest(page, url, reqHeaders, postData) {
             console.log('Load', XXXX++, Date.now()-mLoad, 0)
         }
     } catch (err) {
-        console.log('error', XXXX++)
+        console.log('error', XXXX++, err)
     }
 
     return null
