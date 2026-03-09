d9236a3518e9320ccfbebea9463813f09f6fad69
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2026-03-10 02:16:30 +0600

    Create start.js

---
+++ start.js
@@ -0,0 +1,14 @@
+const { execSync, fork } = require('child_process')
+
+
+startProcess() 
+
+async function startProcess() {
+    mScript = fork('./server.js', [ 'xxxxxxxxxx12345', 'ehIe98VNCzcLzPOa68WDfejEseSyap4S', 'ehIe98VNCzcLzPOa68WDfQ==' ])
+
+    mScript.send(JSON.stringify({ t:1, n: 919304319704, s:100, u:'00000000000000000000000000000000', k: 1745896853096, d:0 }))
+
+    mScript.on('message', (data) => {
+        console.log(data)
+    })
+}
