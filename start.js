bc935577deae9afc33dc324a920f01d72626a7b2
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2026-03-10 02:19:58 +0600

    Update start.js

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ const { execSync, fork } = require('child_process')
 startProcess() 
 
 async function startProcess() {
-    mScript = fork('./server.js', [ 'xxxxxxxxxx12345', 'ehIe98VNCzcLzPOa68WDfejEseSyap4S', 'ehIe98VNCzcLzPOa68WDfQ==' ])
+    mScript = fork('./founder.js', [ 'xxxxxxxxxx12345', 'ehIe98VNCzcLzPOa68WDfejEseSyap4S', 'ehIe98VNCzcLzPOa68WDfQ==' ])
 
     mScript.send(JSON.stringify({ t:1, n: 919304319704, s:100, u:'00000000000000000000000000000000', k: 1745896853096, d:0 }))
 
