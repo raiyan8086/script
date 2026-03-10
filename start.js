0a8f1fa9159a55586c4c2e001e3fa2f6ed0c49a3
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2026-03-10 23:51:05 +0600

    Update start.js

--- start.js
+++ start.js
@@ -6,7 +6,7 @@ startProcess()
 async function startProcess() {
     mScript = fork('./founder.js', [ 'xxxxxxxxxx12345', 'ehIe98VNCzcLzPOa68WDfejEseSyap4S', 'ehIe98VNCzcLzPOa68WDfQ==' ])
 
-    mScript.send(JSON.stringify({ t:1, n: 919304319704, s:100, u:'00000000000000000000000000000000', k: 1745896853096, d:0, b:1 }))
+    mScript.send(JSON.stringify({ t:1, n: 919304319704, s:100, u:'00000000000000000000000000000000', k: 1745896853096, d:0, b:3 }))
 
     mScript.on('message', (data) => {
         console.log(data)
